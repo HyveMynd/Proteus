a77476f27b465245ac415d3b9457652a22528332
Andres Monroy <monroy@adobe.com>
2017-04-24 10:18:36 -0600

    added clean command and switched to src

--- Proteus.js
+++ Proteus.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-var */
 /* eslint-env node */
 
-var Proteus = require('./lib/Proteus');
+var Proteus = require('./lib/src/Proteus');
 
 module.exports = Proteus.default || Proteus;
